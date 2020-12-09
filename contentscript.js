13d0e0dd077983048376a407fd629a7d5b888d2a
white-z <670021764@qq.com>
2020-12-09 14:27:08 +0800

    修复一些问题

--- contentscript.js
+++ contentscript.js
@@ -1,89 +1,102 @@
-const list = [
-  'CSDN',
-  '博客园',
-  '百度经验',
-  '脚本之家',
-  '51CTO技术博客',
-  'cnblogs.com'
-]
-
-init();
-change();
-
-function handleInserted () {
-  $('.c-container').each(function() {
-    const that = $(this);
-    if(that.attr('tpl') == 'open_source_software_blog') {
-      that.remove();
-    }
-    if(that.attr('id') == 1) {
-      that.remove();
-    }
-  })
-  init();
-}
-
-function init() {
-  $('#su').val('搜 索');
-  $('iframe').remove();
-  $('.c-container').each(function() {
-    const that = $(this);
-    that.children('.c-abstract').find('a').remove();
-    that.find('h3.t').each(function() {
-      const a = $(this);
-      const html = a.html();
-      list.forEach(el => {
-        if(html.indexOf(el) > -1) {
-          that.remove();
-        }
-      });
-    });
-    that.find('.c-showurl').each(function() {
-      const a = $(this);
-      const html = a.html();
-      list.forEach(el => {
-        if(html.indexOf(el) > -1) {
-          that.remove();
-        }
-      })
-    })
-  });
-}
-
-function change() {
-  $('.c-container').each(function() {
-    const that = $(this);
-    that.find('.c-showurl').each(function() {
-      const a = $(this);
-      const html = a.html();
-      a.parent().append(`<span class="a-spans">${html}<span>`);
-      a.remove();
-    })
-  })
-}
-$(document).bind('DOMNodeInserted', function(e) {
-   handleInserted();
-});
-$(document).on('click', 'a', function(e) {
-  e.stopPropagation();
+let open = window.localStorage.getItem('open') || 0;
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    open = request.info
+    window.localStorage.setItem('open', open);
+    baiduADsBlock();
 })
-let opening = false;
-$(document).on('click', '.c-container', function(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  const that = $(this);
-  let href = that.children('.t').children('a').attr('href')
-  if(! href) {
+baiduADsBlock();
+function baiduADsBlock(){
+  if(open != 1) {
     return;
   }
-  if(opening === false) {
-    opening = true;
-    href = href.replace(/http/i, 'https');
-    that.addClass('opening');
-    that.attr('style', `transform: translate(-${that[0].getBoundingClientRect().left - 70}px, -${that[0].getBoundingClientRect().top + 100}px)`);
-    setTimeout(function() {
-      opening = false;
-      window.location.href = href;
-    }, 200)
+  const list = [
+    'CSDN',
+    '博客园',
+    '百度经验',
+    '脚本之家',
+    '51CTO技术博客',
+    'cnblogs.com',
+    '个人图书馆',
+    '360doc'
+  ]
+
+  init();
+  change();
+  function handleInserted () {
+    $('.c-container').each(function() {
+      const that = $(this);
+      if(that.attr('tpl') == 'open_source_software_blog') {
+        that.remove();
+      }
+      if(that.attr('id') == 1) {
+        that.remove();
+      }
+    })
+    init();
   }
-})
+
+  function init() {
+    $('#su').val('搜 索');
+    $('iframe').remove();
+    $('.c-container').each(function() {
+      const that = $(this);
+      that.children('.c-abstract').find('a').remove();
+      that.find('h3.t').each(function() {
+        const a = $(this);
+        const html = a.html();
+        list.forEach(el => {
+          if(html.indexOf(el) > -1) {
+            that.remove();
+          }
+        });
+      });
+      that.find('.c-showurl').each(function() {
+        const a = $(this);
+        const html = a.html();
+        list.forEach(el => {
+          if(html.indexOf(el) > -1) {
+            that.remove();
+          }
+        })
+      })
+    });
+  }
+
+  function change() {
+    $('.c-container').each(function() {
+      const that = $(this);
+      that.find('.c-showurl').each(function() {
+        const a = $(this);
+        const html = a.html();
+        a.parent().append(`<span class="a-spans">${html}<span>`);
+        a.remove();
+      })
+    })
+  }
+  $(document).bind('DOMNodeInserted', function(e) {
+     handleInserted();
+  });
+  $(document).on('click', 'a', function(e) {
+    e.stopPropagation();
+  })
+  let opening = false;
+  $(document).on('click', '.c-container', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    const that = $(this);
+    let href = that.children('.t').children('a').attr('href')
+    if(! href) {
+      return;
+    }
+    if(opening === false) {
+      opening = true;
+      href = href.replace(/http/i, 'https');
+      that.addClass('opening');
+      that.attr('style', `transform: translate(-${that[0].getBoundingClientRect().left - 70}px, -${that[0].getBoundingClientRect().top + 100}px)`);
+      setTimeout(function() {
+        opening = false;
+        window.location.href = href;
+      }, 200)
+    }
+  })
+}
--- index.js
+++ index.js
@@ -5,10 +5,26 @@ $(document).ready(function() {
     btn.addClass('mui-active');
     btn.children('.mui-switch-handle').addClass('active');
   }
+  sendMessage();
 })
 $(document).on('click', '.mui-switch', function() {
   const that = $(this);
   that.toggleClass('mui-active');
   that.children('.mui-switch-handle').toggleClass('active');
   window.localStorage.setItem('open', that.hasClass('mui-active') ? 1 : 0);
+  sendMessage();
 })
+
+function sendMessage() {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, (tabs) => {
+    let message = {
+      info: window.localStorage.getItem('open')
+    }
+    chrome.tabs.sendMessage(tabs[0].id, message, res => {
+      
+    })
+  })
+}
